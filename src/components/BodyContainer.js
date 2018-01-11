5b6f3b16fc5dc555284124aead09923e0624daa3
jjwumaster <jjwumaster@gmail.com>
2018-01-11 14:00:27 -0500

    final form

--- src/components/BodyContainer.js
+++ src/components/BodyContainer.js
@@ -13,34 +13,43 @@ class BodyContainer extends Component {
 
   handleCheckbox = e => {
     if (e.target.checked) {
-      const greasedPigs = this.state.displayHogs.filter(pig => {return pig.greased === true})
-      this.setState({displayHogs: greasedPigs})
+      const greasedPigs = this.state.displayHogs.filter(pig => {
+        return pig.greased === true;
+      });
+      this.setState({ displayHogs: greasedPigs });
     } else {
-      this.setState({displayHogs: hogs})
+      this.setState({ displayHogs: hogs });
     }
-  }
+  };
 
   handleDropDown = e => {
-    console.log(e.target.value)
     if (e.target.value === "name") {
       const sortedNames = this.state.displayHogs.sort((a, b) => {
         if (a.name < b.name) {
-          return -1
+          return -1;
         } else {
-          return 1
+          return 1;
         }
       });
-      this.setState({displayHogs: sortedNames})
+      this.setState({ displayHogs: sortedNames });
     } else {
-      const sortedNames = this.state.displayHogs.sort((a, b) => {
-        if (a.weight < b.weight) {
-          return -1
+      const sortedWeights = this.state.displayHogs.sort((a, b) => {
+        const weight =
+          "weight as a ratio of hog to LG - 24.7 Cu. Ft. French Door Refrigerator with Thru-the-Door Ice and Water";
+
+        if (a[weight] < b[weight]) {
+          return 1;
         } else {
-          return 1
+          return -1;
         }
       });
+      this.setState({ displayHogs: sortedWeights });
     }
-  }
+  };
+
+  handleClick = e => {
+    e.target.classList.toggle("flipped");
+  };
 
   render() {
     const imgSrc = "/../hog-imgs/";
@@ -58,16 +67,32 @@ class BodyContainer extends Component {
 
     return (
       <div>
-        Greased: <input onClick={this.handleCheckbox} type="checkbox" value="greased" />
-        <select onChange={this.handleDropDown}>
-          <option value="name">Name</option>
-          <option value="weight">Weight</option>
-        </select>
-        {this.state.displayHogs.map(hog => {
-          return (
-            <Tile hog={hog} key={hog.name} img={formatImgName(hog.name)} />
-          ); //with some props
-        })}
+        <div>
+          Greased:
+          <input
+            onClick={this.handleCheckbox}
+            type="checkbox"
+            value="greased"
+          />
+          <br />
+          <select onChange={this.handleDropDown}>
+            <option value="name">Name</option>
+            <option value="weight">Weight</option>
+          </select>
+        </div>
+        <div> --- </div>
+        <div className="ui grid container">
+          {this.state.displayHogs.map(hog => {
+            return (
+              <Tile
+                hog={hog}
+                key={hog.name}
+                img={formatImgName(hog.name)}
+                handleClick={this.handleClick}
+              />
+            );
+          })}
+        </div>
       </div>
     );
   }
--- src/components/Tile.js
+++ src/components/Tile.js
@@ -1,27 +1,32 @@
 import React from "react";
 
 const Tile = props => {
-  const imgSrc = props.img
+  const imgSrc = props.img;
 
   const weight =
     "weight as a ratio of hog to LG - 24.7 Cu. Ft. French Door Refrigerator with Thru-the-Door Ice and Water";
 
   return (
-    <div className="ui card" key={props.hog.key}>
-      <div className="image">
-        <img src={imgSrc} />
-      </div>
+    <div className="ui eight wide column">
+      <div className="flip-container pigTile">
+        <div key={props.hog.key} onClick={props.handleClick}>
+          <div className="flipper">
+            <div className="front">
+              <img src={imgSrc} alt={props.hog.name} />
+              <h1 className="header">{props.hog.name}</h1>
+            </div>
 
-      <div className="name">
-        <h1 className="header">{props.hog.name}</h1>
-
-        <div className="description">{props.hog.specialty}</div>
-
-        <div className="description">
-          {props.hog.greased ? "greased" : "ungreased"}
+            <div className="back">
+              <div className="description">
+                Specialty: {props.hog.specialty}
+                <br />
+                Status: {props.hog.greased ? "greased" : "ungreased"}
+                <br />
+                Weight: {props.hog[weight]} refrigerators
+              </div>
+            </div>
+          </div>
         </div>
-
-        <div className="description">{props.hog[weight]}</div>
       </div>
     </div>
   );
