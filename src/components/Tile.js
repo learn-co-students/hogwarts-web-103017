b79abcb1040ad47ac1e68834544712974c5dc8c0
Jake <jakemacnaughton@gmail.com>
2018-01-11 10:25:30 -0500

    did some things

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -1,1 +1,28 @@
 import React, { Component } from 'react';
+
+
+
+const Tile = (props) =>
+  <div class="ui card">
+    <div class="image">
+      <img src="/images/avatar2/large/kristy.png">
+    </div>
+    <div class="name">
+      <a class="header">Kristy</a>
+      <div class="meta">
+        <span class="date">Joined in 2013</span>
+      </div>
+      <div class="specialty">
+        Kristy is an art director living in New York.
+      </div>
+      <div class="greased">
+        Kristy is an art director living in New York.
+      </div>
+      <div class="refrigerator">
+        Kristy is an art director living in New York.
+      </div>
+    </div>
+  </div>
+
+
+export default Title;
