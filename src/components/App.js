eeebc08ad17277b60c27f1ec263ba6dbef992c71
psarma89 <priyam.sarma@gmail.com>
2018-01-12 10:54:16 -0500

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -6,8 +6,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-          < Nav />
-
+        < Nav />
+      <div>Also some random text</div>
       </div>
     )
   }
